8c64b6cb5ccb0e2ed45bceec03e3c15f6fed5e46
David <d.boutin84@gmail.com>
2021-06-16 15:54:24 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import {ThemeProvider} from 'next-themes'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    console.log("enter");
+    return (
+        <ThemeProvider>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
