c4c1f1958e1987f8b5db451ed23d02f99e6750ad
Cotetommy <nordik.tommy+1@gmail.com>
2021-06-23 16:14:51 -0400

    env

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,8 +59,8 @@ const Blog: React.FC<{ data: Data }> = ({data}) => {
                     previousLabel='navigate_before'
                     activeLinkClassName={'text-bold text-2xl text-red-500'}
                     previousClassName={'dark:text-white material-icons-outlined pt-0.5 hover:text-red-500'}
-                    nextClassName={'dark:text-white material-icons-outlined pt-0.5 hover:text-red-500'}
-                    pageClassName={'dark:text-white text-lg sm:px-3 mx-2 hover:text-red-500'}
+                    nextClassName={'dark:text-white cursor-pointer select-none material-icons-outlined pt-0.5 hover:text-red-500'}
+                    pageClassName={'dark:text-white cursor-pointer select-none text-lg sm:px-3 mx-2 hover:text-red-500'}
                     breakLinkClassName={'dark:text-white text-lg sm:px-3 mx-2 hover:text-red-500'}
                     disabledClassName={'material-icons-outlined'}
                     forcePage={currentPage - 1 || 0}
