1a26ee66a3fda6861e415145ee7590644c391ef3
Cotetommy <nordik.tommy+1@gmail.com>
2021-06-23 17:11:25 -0400

    env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   env: {
     station_id: 71,
+    NEXT_IMAGE_ALLOWED_DOMAINS: "podboxx-production.s3.amazonaws.com,podboxx-development.s3.amazonaws.com,next.podboxx.com",
+    FORCE_WEBP_OUTPUT: "true"
   },
   reactStrictMode: true,
   target: 'serverless',
