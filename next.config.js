8ab759546a97254b3e9f9fcced300bb6d11b378e
Cotetommy <nordik.tommy+1@gmail.com>
2021-06-23 16:46:42 -0400

    env

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   reactStrictMode: true,
   target: 'serverless',
-  images: {
-    domains: ['source.unsplash.com', 'localhost', 'podboxx-production.s3.amazonaws.com', 'podboxx-development.s3.amazonaws.com', 'next.podboxx.com'],
-  },
+  // images: {
+  //   domains: ['source.unsplash.com', 'localhost', 'podboxx-production.s3.amazonaws.com', 'podboxx-development.s3.amazonaws.com', 'next.podboxx.com'],
+  // },
 }
