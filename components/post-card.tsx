2bfd877a814026fa3fca38fec4dbdb967d4411b2
Cotetommy <nordik.tommy+1@gmail.com>
2021-06-25 08:32:55 -0400

    some fixes + image test

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -17,23 +17,14 @@ const PostCard: React.FC<{ data: Post }> = ({data}) => {
         <>
             <div
                 className=" p-2 w-auto max-w-2xl sm:w-full sm:p-4 h-auto sm:h-56 sm:rounded-2xl red-shadow flex flex-col sm:flex-row gap-5 select-none bg-gray-300 dark:bg-gray-500 dark:bg-cards-dark">
-                {/*{data.img_url ?*/}
                 <Image objectFit='cover' className='rounded-xl' alt={data.title + ' image'} src={data.img_url}
-                       quality='60'
-                       height='h-48 sm:h-48' width='sm:w-48'/>
-                {/*: <>*/}
-                {/*        <Image objectFit='cover' className='rounded-xl' alt={data.title + ' image'}*/}
-                {/*               src='/header-card.png'*/}
-                {/*               quality='60'*/}
-                {/*               height='h-48 sm:h-48' width='sm:w-48'/>*/}
-                {/*    </>*/}
-                {/*}*/}
+                       quality='60' height='h-48 sm:h-48' width='sm:w-48' unoptimized={true}/>
                 <div className="flex sm:flex-1 flex-col gap-2 p-1">
                     <h1 className="text-lg sm:text-lg font-semibold dark:text-gray-300 line-clamp-3 overflow-hidden overflow-ellipsis">
                         {data.title}
                     </h1>
                     <p className="dark:text-gray-400 text-black text-sm sm:text-base">
-                        {Moment.utc(data.publication_date).format('MMMM d YYYY')}
+                        {Moment.utc(data.publication_date).format('MMMM D YYYY')}
                     </p>
                     <div className="flex gap-5 mt-auto">
                         <button
